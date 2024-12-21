e15c0f21792621223ae8b364da0784e4696ab12e
lurkyphish2085 <95119560+Lurky-phish-2085@users.noreply.github.com>
2024-12-21 12:51:08 +0800

    Implement default location fetching based on user's IP address

--- src/api/ipToCountryApi.ts
+++ src/api/ipToCountryApi.ts
@@ -1,8 +1,11 @@
 import axios from "axios";
+import { CountryIsData } from "../types";
 
 const API_URL = "https://api.country.is/";
 
-export const getIPCountry = async (ip: string = "") => {
+export const getCountryByIP = async (
+  ip: string = ""
+): Promise<CountryIsData> => {
   const response = await axios.get(`${API_URL}${ip}`);
 
   return response.data;

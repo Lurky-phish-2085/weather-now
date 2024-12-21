9cc5749163d73ad4abf93a72a07bc5707b6ccfe8
lurkyphish2085 <95119560+Lurky-phish-2085@users.noreply.github.com>
2024-12-21 17:06:29 +0800

    Improve Default Location Fetching Based on User IP
    This update enhances the previous implementation. Instead of using the country
    as the location for fetching weather data, the feature now uses the country's
    capital city. This change ensures that the retrieved weather forecast is more
    accurate and location-specific.

---
+++ src/api/countryApi.ts
@@ -0,0 +1,43 @@
+import axios from "axios";
+import { CountryData } from "../types";
+
+const API_URL = "/countries.json";
+
+export const getCountryData = async (
+  countryName: string
+): Promise<CountryData> => {
+  const response = await axios.get(API_URL);
+  const countries: CountryData[] = response.data;
+
+  const retrievedCountry: CountryData | undefined = countries.find(
+    (country) => country.name === countryName || country.code === countryName
+  );
+
+  if (!retrievedCountry) {
+    throw new Error(`${countryName} is not found.`);
+  }
+
+  return retrievedCountry;
+};
+
+export const getCapitalCityOf = async (
+  countryName: string
+): Promise<string> => {
+  const country = await getCountryData(countryName).catch((error) => {
+    console.log(error);
+    throw new Error(error);
+  });
+
+  return country.capitalCity;
+};
+
+export const getCountryAbbrOf = async (
+  countryName: string
+): Promise<string> => {
+  const country = await getCountryData(countryName).catch((error) => {
+    console.log(error);
+    throw new Error(error);
+  });
+
+  return country.code;
+};
--- src/api/ipToCountryApi.ts
+++ src/api/ipToCountryApi.ts
@@ -1,11 +1,11 @@
 import axios from "axios";
-import { CountryIsData } from "../types";
+import { CountryIPData } from "../types";
 
 const API_URL = "https://api.country.is/";
 
 export const getCountryByIP = async (
   ip: string = ""
-): Promise<CountryIsData> => {
+): Promise<CountryIPData> => {
   const response = await axios.get(`${API_URL}${ip}`);
 
   return response.data;

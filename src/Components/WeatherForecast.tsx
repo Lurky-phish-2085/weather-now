392c20049c591c99c3f77de86c856a51bcf6e304
lurkyphish2085 <95119560+Lurky-phish-2085@users.noreply.github.com>
2024-12-31 21:22:28 +0800

    Extract the forecast overview as a component

--- src/Components/WeatherForecast.tsx
+++ src/Components/WeatherForecast.tsx
@@ -1,4 +1,8 @@
 import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
+import dayjs, { Dayjs } from "dayjs";
+import customParseFormat from "dayjs/plugin/customParseFormat";
+import timezonePlug from "dayjs/plugin/timezone";
+import utc from "dayjs/plugin/utc";
 import isEmpty from "lodash.isempty";
 import { useEffect } from "react";
 import { useAppContext } from "../Contexts/hooks";
@@ -8,6 +12,10 @@ import { getCountryByIP } from "../api/ipToCountryApi";
 import { getWeatherForecast } from "../api/weatherApi";
 import { getWMOCodeInterpretation } from "../api/wmoInterpretApi";
 import SkeletonScreen from "./SkeletonScreen";
+
+dayjs.extend(utc);
+dayjs.extend(timezonePlug);
+dayjs.extend(customParseFormat);
 
 function WeatherForecast() {
   const { location, selectLocation, temperatureUnit } = useAppContext();
@@ -56,73 +64,128 @@ function WeatherForecast() {
     <>
       {isEmpty(location) || isLoading ? <SkeletonScreen /> : <></>}
       {data ? (
-        <>
-          <div className="page bottom active">
-            <div className="grid">
-              <div className="s6">
-                <h4>{location.display_name}</h4>
-              </div>
-              <div className="s6 right-align">
-                <h4>NOW</h4>
-              </div>
-            </div>
-            <hr className="small" />
-            <div className="grid">
-              <div className="s6">
-                <h1>
-                  {data.current.temperature_2m}
-                  <span>&thinsp;</span>
-                  {data.current_units.temperature_2m}
-                </h1>
-                <div>
-                  <div>
-                    Feels Like: {data.current.apparent_temperature}
-                    {data.current_units.apparent_temperature}
-                  </div>
-                  <div>
-                    Precipitation: {data.current.precipitation}
-                    <span>&thinsp;</span>
-                    {data.current_units.precipitation}
-                  </div>
-                  <div>
-                    Humidity: {data.current.relative_humidity_2m}
-                    {data.current_units.relative_humidity_2m}
-                  </div>
-                  <div>
-                    Wind Speed: {data.current.wind_speed_10m}
-                    <span>&thinsp;</span>
-                    {data.current_units.wind_speed_10m}
-                  </div>
-                </div>
-              </div>
-              <div className="s6 right-align">
-                <h4>
-                  {
-                    wmoCodeInterpretation.data?.[
-                      data.current.is_day ? "day" : "night"
-                    ].description
-                  }
-                </h4>
-                <img
-                  src={
-                    wmoCodeInterpretation.data?.[
-                      data.current.is_day ? "day" : "night"
-                    ].image
-                  }
-                  alt={
-                    wmoCodeInterpretation.data?.[
-                      data.current.is_day ? "day" : "night"
-                    ].description
-                  }
-                />
-              </div>
-            </div>
-          </div>
-        </>
+        <ForecastOverview
+          locationName={location.display_name}
+          date={dayjs.utc(data.current.time)}
+          timezone={data.timezone}
+          temp={data.current.temperature_2m}
+          feelsLikeTemp={data.current.apparent_temperature}
+          interpretation={
+            wmoCodeInterpretation.data?.[data.current.is_day ? "day" : "night"]
+              .description
+          }
+          interpretationImgSource={
+            wmoCodeInterpretation.data?.[data.current.is_day ? "day" : "night"]
+              .image
+          }
+          interpretationImgAlt={
+            wmoCodeInterpretation.data?.[data.current.is_day ? "day" : "night"]
+              .description
+          }
+          precipitation={data.current.precipitation}
+          humidity={data.current.relative_humidity_2m}
+          windSpeed={data.current.wind_speed_10m}
+          units={{
+            tempUnit: data.current_units.temperature_2m,
+            speedUnit: data.current_units.wind_speed_10m,
+          }}
+        />
       ) : (
         <></>
       )}
     </>
+  );
+}
+
+type ForecastOverviewProps = {
+  locationName: string;
+  date: Dayjs;
+  timezone: string;
+  temp: number;
+  feelsLikeTemp: number;
+  interpretation: string | undefined;
+  interpretationImgSource: string | undefined;
+  interpretationImgAlt?: string | undefined;
+  precipitation: number;
+  humidity: number;
+  windSpeed: number;
+  units: {
+    tempUnit: string;
+    speedUnit: string;
+  };
+};
+
+function ForecastOverview({
+  locationName,
+  date,
+  timezone,
+  temp,
+  feelsLikeTemp,
+  interpretation,
+  interpretationImgSource,
+  interpretationImgAlt = "",
+  precipitation,
+  humidity,
+  windSpeed,
+  units,
+}: ForecastOverviewProps) {
+  const day = date.isSame(dayjs.tz(dayjs(), timezone), "day")
+    ? "NOW"
+    : date.format("ddd");
+
+  return (
+    <div className="page bottom active">
+      <div className="grid">
+        <div className="s6">
+          <h4>{locationName}</h4>
+        </div>
+        <div className="s6 right-align middle-align">
+          <h4>{day}</h4>
+        </div>
+      </div>
+      <hr className="small" />
+      <div className="grid">
+        <div className="s6">
+          <h1>
+            {temp}
+            <span>&thinsp;</span>
+            {units.tempUnit}
+          </h1>
+          <div>
+            <div>
+              Feels Like:<span>&nbsp;</span>
+              {feelsLikeTemp}
+              <span>&thinsp;</span>
+              {units.tempUnit}
+            </div>
+          </div>
+          <div>
+            <div>
+              Precipitation:<span>&nbsp;</span>
+              {precipitation}%
+            </div>
+          </div>
+          <div>
+            <div>
+              Humidity:<span>&nbsp;</span>
+              {humidity}%
+            </div>
+          </div>
+          <div>
+            <div>
+              Wind Speed:<span>&nbsp;</span>
+              {windSpeed}
+              <span>&thinsp;</span>
+              {units.speedUnit}
+            </div>
+          </div>
+        </div>
+        <div className="s6 right-align">
+          <h4>{interpretation}</h4>
+          <img src={interpretationImgSource} alt={interpretationImgAlt} />
+        </div>
+      </div>
+    </div>
   );
 }
 

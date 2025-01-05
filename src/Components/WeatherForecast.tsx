d099acbb1b630e816f373441c8e260b340c8e0f8
lurkyphish2085 <95119560+Lurky-phish-2085@users.noreply.github.com>
2025-01-05 10:16:36 +0800

    Use `DailyForecasts`  in `WeatherForecast` component
    Users will now be able to look up future weather forecasts.

--- src/Components/WeatherForecast.tsx
+++ src/Components/WeatherForecast.tsx
@@ -12,6 +12,7 @@ import { searchLocations } from "../api/geocodingApi";
 import { getCountryByIP } from "../api/ipToCountryApi";
 import { getWeatherForecast } from "../api/weatherApi";
 import { ForecastOverviewData } from "../types";
+import DailyForecasts from "./DailyForecasts";
 import ForecastOverview from "./ForecastOverview";
 import SkeletonScreen from "./SkeletonScreen";
 
@@ -22,7 +23,15 @@ dayjs.extend(customParseFormat);
 function WeatherForecast() {
   const { location, selectLocation, temperatureUnit } = useAppContext();
 
-  const [overview, setOverview] = useState({} as ForecastOverviewData);
+  const [currentOverview, setCurrentOverview] = useState(
+    {} as ForecastOverviewData
+  );
+  const [selectedOverview, setSelectedOverview] = useState(
+    {} as ForecastOverviewData
+  );
+  const [dailyForecastOverview, setDailyForecastOverview] = useState(
+    [] as ForecastOverviewData[]
+  );
 
   const queryClient = useQueryClient();
   const { isLoading, data } = useQuery({
@@ -72,33 +81,76 @@ function WeatherForecast() {
       });
     }
 
-    setOverview({
+    setCurrentOverview({
       locationName: location.display_name,
       date: dayjs.utc(data.current.time),
       timezone: data.timezone,
       isDay: data.current.is_day,
       temperature: data.current.temperature_2m,
+      temperatureMax: 0,
+      temperatureMin: 0,
       feelsLikeTemp: data.current.apparent_temperature,
+      feelsLikeTempMax: 0,
+      feelsLikeTempMin: 0,
       wmoCode: data.current.weather_code,
       precipitation: data.current.precipitation,
+      precipitationMax: 0,
       humidity: data.current.relative_humidity_2m,
       windSpeed: data.current.wind_speed_10m,
+      windSpeedMax: 0,
       units: {
         tempUnit: data.current_units.temperature_2m,
         speedUnit: data.current_units.wind_speed_10m,
       },
     });
+    setDailyForecastOverview(
+      data.daily.time.map((date, index) => {
+        return {
+          locationName: location.display_name,
+          date: dayjs.utc(date),
+          timezone: data.timezone,
+          isDay: 1,
+          temperature: data.daily.temperature_2m_max[index],
+          temperatureMax: data.daily.temperature_2m_max[index],
+          temperatureMin: data.daily.temperature_2m_min[index],
+          feelsLikeTemp: data.daily.apparent_temperature_max[index],
+          feelsLikeTempMax: data.daily.apparent_temperature_max[index],
+          feelsLikeTempMin: data.daily.apparent_temperature_min[index],
+          wmoCode: data.daily.weather_code[index],
+          precipitation: data.daily.precipitation_probability_max[index],
+          precipitationMax: data.daily.precipitation_probability_max[index],
+          humidity: 0,
+          windSpeed: data.daily.wind_speed_10m_max[index],
+          windSpeedMax: data.daily.wind_speed_10m_max[index],
+          units: {
+            tempUnit: data.current_units.temperature_2m,
+            speedUnit: data.current_units.wind_speed_10m,
+          },
+        } as ForecastOverviewData;
+      })
+    );
   }, [data, location, temperatureUnit]);
 
   return (
     <>
-      {isEmpty(location) || isEmpty(overview) || isLoading ? (
+      {isEmpty(location) || isEmpty(currentOverview) || isLoading ? (
         <SkeletonScreen />
       ) : (
         <></>
       )}
-      {data && !isEmpty(overview) ? (
-        <ForecastOverview data={overview} />
+      {data && !isEmpty(currentOverview) ? (
+        <>
+          {isEmpty(selectedOverview) ? (
+            <ForecastOverview data={currentOverview} />
+          ) : (
+            <ForecastOverview data={selectedOverview} />
+          )}
+          <DailyForecasts
+            onSelect={(data) => setSelectedOverview(data)}
+            selected={currentOverview}
+            data={dailyForecastOverview}
+          />
+        </>
       ) : (
         <></>
       )}
